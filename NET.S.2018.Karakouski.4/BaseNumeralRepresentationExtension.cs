2bb25bf2033c9d4f424b76f8277796dc9ee917cd
uu <uu@U>
2018-03-22 10:13:03 +0300

    Initial commit

---
+++ NET.S.2018.Karakouski.4/BaseNumeralRepresentationExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NET.S._2018.Karakouski._4
+{
+    public static class BaseNumeralRepresentationExtension
+    {
+        public static string BinaryRepresentation(this double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Base10Number(this string number, int nBase)
+        {
+            if (nBase < 2 || nBase > 16)
+                throw new ArgumentOutOfRangeException(nameof(nBase));
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NET.S.2018.Karakouski.4/GcdCalulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NET.S._2018.Karakouski._4
+{
+    public static class GcdCalulator
+    {
+        public static int EuclidGcdCalc(int []args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int BinaryGcdCalc(int[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
