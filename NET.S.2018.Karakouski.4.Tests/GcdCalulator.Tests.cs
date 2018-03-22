3284f9457cd2e3a0628f5bb30965291c4267c7cd
uu <uu@U>
2018-03-22 11:18:26 +0300

    BinaryGcdCalc method implimentation and tests + minor fixes

--- NET.S.2018.Karakouski.4.Tests/GcdCalulator.Tests.cs
+++ NET.S.2018.Karakouski.4.Tests/GcdCalulator.Tests.cs
@@ -10,12 +10,24 @@ namespace NET.S._2018.Karakouski._4.Tests
         [TestCase(6, 2, ExpectedResult = 2)]
         [TestCase(12, 3, 9, ExpectedResult = 3)]
         [TestCase(36, 12, 3, 9, ExpectedResult = 3)]
-        public double EuclidianMethod_PoistiveNumbers_TestCalulations(params int[] args) => GcdCalulator.EuclidGcdCalc(args);
+        public int EuclidianMethod_PoistiveNumbers_TestCalulations(params int[] args) => GcdCalulator.EuclidGcdCalc(args);
 
         [TestCase(2, -3, ExpectedResult = 1)]
         [TestCase(-6, 2, ExpectedResult = 2)]
         [TestCase(-12, 3, 9, ExpectedResult = 3)]
         [TestCase(36, -12, -3, 9, ExpectedResult = 3)]
-        public double EuclidianMethod_PoistiveAndNegativeNumbers_TestCalulations(params int[] args) => GcdCalulator.EuclidGcdCalc(args);
+        public int EuclidianMethod_PoistiveAndNegativeNumbers_TestCalulations(params int[] args) => GcdCalulator.EuclidGcdCalc(args);
+
+        [TestCase(2, 3, ExpectedResult = 1)]
+        [TestCase(6, 2, ExpectedResult = 2)]
+        [TestCase(12, 3, 9, ExpectedResult = 3)]
+        [TestCase(36, 12, 3, 9, ExpectedResult = 3)]
+        public int BinaryMethod_PoistiveNumbers_TestCalulations(params int[] args) => GcdCalulator.BinaryGcdCalc(args);
+
+        [TestCase(2, -3, ExpectedResult = 1)]
+        [TestCase(-6, 2, ExpectedResult = 2)]
+        [TestCase(-12, 3, 9, ExpectedResult = 3)]
+        [TestCase(36, -12, -3, 9, ExpectedResult = 3)]
+        public int BinaryMethod_PoistiveAndNegativeNumbers_TestCalulations(params int[] args) => GcdCalulator.BinaryGcdCalc(args);
     }
 }
--- NET.S.2018.Karakouski.4/GcdCalulator.cs
+++ NET.S.2018.Karakouski.4/GcdCalulator.cs
@@ -2,8 +2,16 @@
 
 namespace NET.S._2018.Karakouski._4
 {
+    /// <summary>
+    /// Impliments Euclide and Binary method of GCD calculation
+    /// </summary>
     public static class GcdCalulator
     {
+        /// <summary>
+        /// Impliments Euclide of GCD calculation
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static int EuclidGcdCalc(params int[] args)
         {
             int gcd = args[0];
@@ -16,16 +24,59 @@ namespace NET.S._2018.Karakouski._4
             return gcd;
          }
 
+        /// <summary>
+        /// Impliments recursive part of Euclide of GCD calculation
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         private static int EuclidGcdCalc(int a, int b)
         {
             if (b == 0)
                 return Math.Abs(a);
             return EuclidGcdCalc(b, a % b);
         }
-
+        /// <summary>
+        /// Impliments Binary method of GCD calculation
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static int BinaryGcdCalc(params int[] args)
         {
-            throw new NotImplementedException();
+            int gcd = args[0];
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                gcd = BinaryGcdCalc(gcd, args[i]);
+            }
+
+            return gcd;
+
+        }
+
+        /// <summary>
+        /// Impliments recursive part of Binary of GCD calculation
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static int BinaryGcdCalc(int a, int b)
+        {
+            if (a == 0) return b;
+            if (b == 0) return a;
+
+            if ((b & 1) == 0 && (a & 1) == 0) return BinaryGcdCalc(b >> 1, a >> 1) << 1;
+
+            // a is even, b is odd
+            else if ((b & 1) == 0) return BinaryGcdCalc(b >> 1, a);
+
+            // a is odd, b is even
+            else if ((a & 1) == 0) return BinaryGcdCalc(b, a >> 1);
+
+            // a and b odd, a >= b
+            else if (b >= a) return BinaryGcdCalc((b - a) >> 1, a);
+
+            // a and b odd, a < b
+            else return BinaryGcdCalc(b, (a - b) >> 1);
         }
     }
 }
