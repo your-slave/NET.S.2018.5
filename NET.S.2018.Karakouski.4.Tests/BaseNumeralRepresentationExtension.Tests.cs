6dc0f8d433879d174e72bad81082613ce9532dae
John Smith <john@smith.com>
2018-03-23 12:33:34 +0300

    GetNumeric10BaseRespresentIon method and tests implimentation

--- NET.S.2018.Karakouski.4.Tests/BaseNumeralRepresentationExtension.Tests.cs
+++ NET.S.2018.Karakouski.4.Tests/BaseNumeralRepresentationExtension.Tests.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using NET.S._2018.Karakouski._4;
+using System;
 
 namespace NET.S._2018.Karakouski._4.Tests
 {
+    [TestFixture]
     public class BaseNumeralRepresentationExtensionTests
     {
+        /*
+"0110111101100001100001010111111" для основания 2 -> 934331071
+"01101111011001100001010111111" для основания 2 -> 233620159
+"11101101111011001100001010" для основания 2 -> 62370570
+"1AeF101" для основания 2 -> ArgumentException
+"11111111111111111111111111111111" для основания 2 -> OverflowException
+"1AeF101" для основания 16 -> 28242177
+"1ACB67" для основания 16 -> 1756007
+"SA123" для основания 2 -> ArgumentException
+"764241" для основания 8 -> 256161
+"764241" для основания 20 -> ArgumentException
+"10" для основания 5 -> 5
+        */
+        [TestCase("0110111101100001100001010111111", 2, ExpectedResult = "934331071")]
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = "233620159")]
+        [TestCase("11101101111011001100001010", 2, ExpectedResult = "62370570")]
+        [TestCase("1AeF101", 16, ExpectedResult = "28242177")]
+        [TestCase("1ACB67", 16, ExpectedResult = "1756007")]
+        [TestCase("764241", 8, ExpectedResult = "256161")]
+        [TestCase("10", 5, ExpectedResult = "5")]
+        public string Base10Number_NormalInputs_TestCalulations(string number, int nBase) => number.Base10Number(nBase);
+
+        [TestCase("1AeF101", 2)]
+        public void Base10Number_StringHavingSymbolsOutOfTheBase_ArgumentException(string number, int nBase)
+        {
+            Assert.Throws<ArgumentException>(() => number.Base10Number(nBase));
+        }
+
+        [TestCase("11111111111111111111111111111111", 2)]
+        public void Base10Number_TooBigInputValue_OverflowException(string number, int nBase)
+        {
+            Assert.Throws<OverflowException>(() => number.Base10Number(nBase));
+        }
+
+        [TestCase("SA123", 2)]
+        public void Base10Number_InvalidSymbolInTheInputString_ArgumentException(string number, int nBase)
+        {
+            Assert.Throws<ArgumentException>(() => number.Base10Number(nBase));
+        }
+
+        [TestCase("764241", 20)]
+        public void Base10Number_UnsupportedBase_ArgumentOutOfRangeException(string number, int nBase)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => number.Base10Number(nBase));
+        }
     }
 }
--- NET.S.2018.Karakouski.4/BaseNumeralRepresentationExtension.cs
+++ NET.S.2018.Karakouski.4/BaseNumeralRepresentationExtension.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace NET.S._2018.Karakouski._4
 {
     public static class BaseNumeralRepresentationExtension
     {
+        private static string legal6BaseChars = "FEDCBA9876543210";
+
         public static string BinaryRepresentation(this double number)
         {
+            //1025 (decimal) to base 15:
+            //1025 / 15 = 68 , remainder 5
+            //68 / 15 = 4 , remainder 8
+            //4 / 15 = 0 , remainder 4
+
+
+            //Поэтому уже в самых первых машинах начали использовать трюк, делая первый бит мантиссы всегда положительным. Такое предаставление назвали нормализованным.
+
+            //aftrwer , the same but with minus
+
             throw new NotImplementedException();
         }
 
@@ -13,7 +27,58 @@ namespace NET.S._2018.Karakouski._4
         {
             if (nBase < 2 || nBase > 16)
                 throw new ArgumentOutOfRangeException(nameof(nBase));
-            throw new NotImplementedException();
+
+            number = number.ToUpper();
+
+            if (!number.All(c => legal6BaseChars.Substring(nBase-1).Contains(c)))
+            {
+                throw new ArgumentException(nameof(number));
+            }
+
+            int result = 0;
+
+            for(int i=0; i< number.Length; i++)
+            {
+                checked { result += number[i].GetNumeric10BaseRespresentIon() * (int)Math.Pow(nBase, number.Length - 1 - i); }
+            }
+
+            return result.ToString();
+        } 
+        
+        private static int GetNumeric10BaseRespresentIon(this char symbol)
+        {
+            if(Char.IsDigit(symbol))
+                return (int)Char.GetNumericValue(symbol);
+
+            switch (symbol)
+            {
+                case ('A'):
+                    {
+                        return 10;
+                    }
+                case ('B'):
+                    {
+                        return 11;
+                    }
+                case ('C'):
+                    {
+                        return 12;
+                    }
+                case ('D'):
+                    {
+                        return 13;
+                    }
+                case ('E'):
+                    {
+                        return 14;
+                    }
+                case ('F'):
+                    {
+                        return 15;
+                    }
+            }
+
+            throw new ArgumentException();
         }
     }
 }
